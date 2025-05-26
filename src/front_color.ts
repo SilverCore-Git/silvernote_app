bd9c467e877b077276b967232c2867df7cfde23e
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-05-26 18:57:39 +0200

    revert color

---
+++ src/front_color.ts
@@ -0,0 +1,15 @@
+
+const color = () => { return {
+
+    default: {
+        btn: '#AEE2FF',
+        back2: '#D96E30',
+        text: '#3B3B3B',
+        back: '#FFF8F0',
+        danger: '#F28C28'
+    } 
+
+}};
+
+
+export default color;
