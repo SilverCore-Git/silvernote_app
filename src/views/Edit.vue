3abce3333dc8960a257bce3b1cfd620bf72fa9b6
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-05-27 19:15:49 +0200

    add choice for simply text editing

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -2,22 +2,40 @@
 
   <header class="flex flex-row pt-4 relative">
     <div class="left-arrow absolute left-4" @click="router.push('/')"></div>
-    <div
-      class="pin absolute right-4"
-      :style="{
-        backgroundImage: if_pin_active
-          ? `url(${pinFull})`
-          : `url(${pinEmpty})`
-      }"
-      @click="change_pin_state"
-    ></div>
+
+    <div class="flex flex-row gap-4 absolute right-4">
+
+      <div
+        class="edit_note"
+        :style="{
+          backgroundImage: if_edit_note_active
+            ? `url(${edit_note_Full})`
+            : `url(${edit_note_Empty})`
+        }"
+        @click="if_edit_note_active = !if_edit_note_active"
+      ></div>
+
+      <div
+        class="pin"
+        :style="{
+          backgroundImage: if_pin_active
+            ? `url(${pinFull})`
+            : `url(${pinEmpty})`
+        }"
+        @click="change_pin_state"
+      ></div>
+
+    </div>
+
   </header>
 
   <section class="flex flex-col justify-center items-center h-full w-[100%] mt-12 overflow-x-hidden">
 
     <input class="text-3xl mb-3 font-bold" type="text" placeholder="Titre...">
 
-    <RichMarkdownEditor  />
+    <RichMarkdownEditor v-if="if_edit_note_active" />
+
+    <MarkdownEditor v-else />
 
   </section>
 
@@ -35,11 +53,22 @@ const router = useRouter();
 const route = useRoute();
 
 import RichMarkdownEditor from '../components/RichMarkdownEditor.vue';
+import MarkdownEditor from '../components/MarkdownEditor.vue';
 
 import pinFull from '../assets/svgs/pin_plein.png?url';
 import pinEmpty from '../assets/svgs/pin_vide.png?url';
 
 const if_pin_active = ref(route.query.pinned == "true" ? true : false);
+
+// import MathFull from '../assets/svgs/calculation.png?url';
+// import MathEmpty from '../assets/svgs/calculation.png?url';
+
+const if_math_active = ref(false);
+
+import edit_note_Full from '../assets/svgs/note-edit_plein.png?url';
+import edit_note_Empty from '../assets/svgs/note-edit_vide.png?url';
+
+const if_edit_note_active = ref(true);
 
 const change_pin_state = () => {
     if_pin_active.value = !if_pin_active.value;
@@ -61,7 +90,9 @@ const change_pin_state = () => {
 <style scoped>
 
 .left-arrow,
-.pin {
+.pin,
+.math,
+.edit_note {
   width: 30px;
   height: 30px;
   background-size: contain;
