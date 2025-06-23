30637539e9dc7102e71792fd6d510cc13732911c
mister_papaye <philippeetchtebaise@hotmail.com>
2025-06-23 18:19:18 +0200

    maj

---
+++ src/components/Nav_bar.vue
@@ -0,0 +1,30 @@
+<template>
+    
+    <div class="flex flex-row justify-between items-center">
+
+        <img class="ml-5 w-13" src="/favicon.svg" alt="logo">
+
+        <ul class=" flex flex-row gap-8">
+            <li id="goto_home"><a>Maison</a></li>
+            <li id="goto_simply"><a>Simple et épuré</a></li>
+            <li id="goto_everywhere"><a>Vos notes partout</a></li>
+            <li id="goto_offline"><a>Hors connection</a></li>
+            <li id="goto_editor"><a>Editeur de texte</a></li>
+            <li id="goto_demo"><a>Essayer</a></li>
+        </ul>
+
+        <div>
+
+            <button class="second mr-4">
+                Connexion
+            </button>
+
+            <button class="primary mr-10">
+                Essayer silvernote gratuitement
+            </button>
+
+        </div>
+
+    </div>
+
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>v</h1>
+
+    <header class="absolute top-3 right-0 left-0">
+        <Nav_bar />
+    </header>
+
+    
+    <section id="home">
+
+        <h1 class=" uppercase font-bold " style="letter-spacing: 2px;">silvernote</h1>
+        <h2 class=" text-2xl uppercase font-bold" style="letter-spacing: 9.5px;"><i class="text-[var(--btn)]">simple</i> et <i class="text-[var(--btn)]">cool</i></h2>
+
+    </section>
+
+
 </template>
+
+<script lang="ts" setup>
+
+import Nav_bar from '../components/Nav_bar.vue';
+
+</script>
