adc7414db60cf4a36e5446244e6bc5145c960f3f
mister_papaye <philippeetchtebaise@hotmail.com>
2025-06-23 18:45:24 +0200

    add swaper paniere

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,30 @@
     </header>
 
     
-    <section id="home">
+    <section id="home" class="flex justify-center items-center flex-col">
 
         <h1 class=" uppercase font-bold " style="letter-spacing: 2px;">silvernote</h1>
         <h2 class=" text-2xl uppercase font-bold" style="letter-spacing: 9.5px;"><i class="text-[var(--btn)]">simple</i> et <i class="text-[var(--btn)]">cool</i></h2>
+
+
+
+  <swiper class="w-full" :spaceBetween="0" :slidesPerView="3" loop>
+    <swiper-slide>
+      <img src="/assets/img/banner.jpg" alt="Banner 1" class="w-full h-auto" />
+    </swiper-slide>
+    <swiper-slide>
+      <img src="/assets/img/banner.jpg" alt="Banner 2" class="w-full h-auto" />
+    </swiper-slide>
+    <swiper-slide>
+      <img src="/assets/img/banner.jpg" alt="Banner 3" class="w-full h-auto" />
+    </swiper-slide>
+    <swiper-slide>
+      <img src="/assets/img/banner.jpg" alt="Banner 4" class="w-full h-auto" />
+    </swiper-slide>
+    <swiper-slide>
+      <img src="/assets/img/banner.jpg" alt="Banner 5" class="w-full h-auto" />
+    </swiper-slide>
+  </swiper>
 
     </section>
 
@@ -16,6 +36,9 @@
 </template>
 
 <script lang="ts" setup>
+
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
 
 import Nav_bar from '../components/Nav_bar.vue';
 
