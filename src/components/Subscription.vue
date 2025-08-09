2e8771f3a657ef2f59f5b6efd5d098dd11295333
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-08-09 16:41:48 +0200

    add subscriprions management on user profile

--- src/components/Subscription.vue
+++ src/components/Subscription.vue
@@ -1,129 +1,195 @@
+
 <template>
 
-    <div v-if="loaded && plan" class="w-full bg-white rounded-2xl shadow-xl p-6 border border-gray-200 hover:shadow-2xl transition-all duration-300">
-        
-        <div class="flex items-center justify-between">
+    <div 
+        class="w-full space-y-5"
+    >
 
-            <h2 class="text-2xl font-bold text-yellow-600">
-                {{ plan.name }} Plan
-            </h2>
+        <section 
+            v-if="subscriptions && localuser && !loading"
+            v-for="sub in subscriptions" :key="sub.id"    
+            class="bg-[var(--bg2)] rounded-2xl shadow-xl p-6 border border-gray-200 hover:shadow-2xl transition-all duration-300"
+        >
+            
+            <div class="flex items-center justify-between">
 
-            <span class="text-sm bg-yellow-100 text-yellow-800 px-3 py-1 rounded-full uppercase tracking-wide">
-                {{ plan.plan_data.each }}
-            </span>
+                <h2 class="text-2xl font-bold" :style="{ color: plan_color }">
+                    {{ localuser.plan.name }} Plan
+                </h2>
 
-        </div>
+                <span 
+                    class="text-sm bg-yellow-100 px-3 py-1 rounded-full uppercase tracking-wide"
+                    :style="{ color: plan_color }"
+                >
+                    {{ sub.items.data[0].price.unit_amount / 100 }} 
+                    {{ sub.items.data[0].price.currency.toUpperCase() }} / 
+                    {{ sub.items.data[0].price.recurring.interval }}
+                </span>
 
-        <div class="mt-4 space-y-2 text-gray-700 text-sm">
-                
-            <div class="flex items-center gap-2">
-                
-                <svg class="h-5 w-5 text-gray-500" fill="none" stroke="currentColor" stroke-width="2"
-                    viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M12 8c-1.657 0-3 1.343-3 3s1.343 3 3 3 3-1.343 3-3-1.343-3-3-3z" />
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M12 14v7m0-13V3m6.364 3.636l1.414 1.414M4.222 4.222l1.414 1.414M21 12h-7m-7 0H3m3.636 6.364l-1.414 1.414m13.556-1.414l-1.414 1.414" />
-                </svg>
-
-                <p>
-                    <strong>Formule:</strong>
-                    {{ plan.plan_data.family ? 'Famille 👨‍👩‍👧‍👦' : 'Individuelle 👤' }}
-                </p>
-                
             </div>
 
-            <div class="text-gray-400 text-xs mt-4 truncate">
-                <span><strong>SUBSCRIPTIONS_ID :</strong> {{ plan.plan_data.strip_session_id || "undefined" }}</span><br />
-                <span><strong>PLAN_ID :</strong> {{ plan.uuid || "undefined" }}</span>
-            </div>
-
-        </div>
-    
-    </div>
-
-    <div v-if="!loaded" class="w-full bg-white rounded-2xl shadow-xl p-6 border border-gray-200 hover:shadow-2xl transition-all duration-300">
-        
-        <div class="flex items-center justify-between">
-
-            <h2 class="text-2xl font-bold text-yellow-600" :class="loading"></h2>
-
-            <span class="text-sm text-yellow-800 px-3 py-1 rounded-full uppercase tracking-wide" :class="loading"></span>
-
-        </div>
-
-        <div class="mt-4 space-y-2 text-gray-700 text-sm">
+            <div class="mt-4 flex justify-between items-end text-gray-700 text-sm">
                 
-            <div class="flex items-center gap-2">
+                <div>
 
-                <p :class="loading" class="w-50">
-                </p>
+                    <div class="flex flex-col">
+
+
+                        <p>
+                            <strong>Status:</strong>
+                            {{ sub.status }}
+                        </p>
+
+                        <p>
+                            <strong>Formule:</strong>
+                            {{ localuser.plan.family ? 'Famille' : 'Individuelle' }}
+                        </p>
+                        
+                    </div>
+
+                    <div class="text-gray-400 text-xs mt-4 truncate">
+                        <span><strong>SUBSCRIPTIONS_ID :</strong> {{ sub.id || "undefined" }}</span><br />
+                        <span><strong>PLAN_ID :</strong> {{ localuser.plan.uuid || "undefined" }}</span>
+                    </div>
+
+                </div>
+
+                <button 
+                    class="perso" 
+                    :style="{ '--btn-color': plan_color }"
+                >
+                    Résilier l'abonnement
+                </button>
+            
+
+            </div>
+
+        </section>
+
+
+        <section
+            v-else
+            v-for="index in 3" 
+            :key="index"
+            class="bg-[var(--bg2)] rounded-2xl shadow-xl p-6 border border-gray-200 hover:shadow-2xl transition-all duration-300"
+        >
+            
+            <div class="flex items-center justify-between">
+
+                <h2 class="text-2xl font-bold animate-pulse bg-gray-300 w-55 h-8 rounded-full" ></h2>
+
+                <span 
+                    class="text-sm px-3 py-1 uppercase tracking-wide
+                    animate-pulse bg-gray-300 w-40 h-6 rounded-full"
+                ></span>
+
+            </div>
+
+            <div class="mt-4 flex justify-between items-end text-gray-700 text-sm">
                 
+                <div>
+
+                    <div class="flex flex-col">
+
+
+                        <p class="animate-pulse bg-gray-300 w-40 h-4 mb-0.5 rounded-full">
+                        </p>
+
+                        <p class="animate-pulse bg-gray-300 w-50 h-4 rounded-full">
+                        </p>
+                        
+                    </div>
+
+                    <div class="text-gray-400 text-xs mt-4 truncate">
+                        <div class="animate-pulse bg-gray-300 h-3 w-110 rounded-full"></div>
+                        <div class="animate-pulse bg-gray-300 h-3 w-90 rounded-full"></div>
+                    </div>
+
+                </div>
+
+                <div class="animate-pulse bg-gray-300 h-11 w-45 rounded-md"></div>
+
             </div>
 
-            <div class="text-gray-400 text-xs mt-4 truncate">
-                <div class="w-150" :class="loading"></div>
+        </section>
+
+
+        <section class="bg-[var(--bg2)] rounded-2xl shadow-xl p-6 border border-gray-200 hover:shadow-2xl transition-all duration-300">
+
+            <h2 class="text-xl font-semibold mb-2">Moyens de paiement</h2>
+
+            <div v-if="paymentMethods.length" >
+                
+                <ul>
+                    
+                    <li v-for="pm in paymentMethods" :key="pm.id" class="py-2">
+                    
+                        {{ pm.card.brand.toUpperCase() }} •••• •••• •••• {{ pm.card.last4 }}
+                        <span class="text-gray-500 text-sm">exp. {{ pm.card.exp_month }}/{{ pm.card.exp_year }}</span>
+                    
+                    </li>
+
+                </ul>
+            
             </div>
 
-        </div>
-    
-    </div>
-    
+            <p v-else class="text-gray-500">Aucun moyen de paiement enregistré.</p>
 
-    <div v-if="!plan && loaded" class="w-full bg-white rounded-2xl shadow-xl p-6 border border-gray-200 hover:shadow-2xl transition-all duration-300">
-    
-        <h2>Aucun plan trouvé</h2>
+        </section>
+
 
     </div>
 
 </template>
 
-<script lang="ts" setup>
 
-import { useUser } from '@clerk/vue';
-import { ref } from 'vue';
 
-const { user, isLoaded } = useUser()
+<script setup>
 
-const plan = ref();
-const loaded = ref<boolean>(false)
-const loading = 'animate-pulse bg-gray-300 w-28 h-6 rounded-full';
+import { ref, onMounted } from 'vue'
+import { useUser } from '@clerk/vue'
 
-const interval = setInterval(async () => {
+const customer = ref(null);
+const localuser = ref(null);
+const paymentMethods = ref([]);
+const subscriptions = ref([]);
+const loading = ref(true);
+const error = ref(null);
+const plan_color = ref('');
 
-  if (isLoaded) {
-    
-    try {
-    
-        const res = await fetch('http://localhost:3000/user/get/data', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ user_id: user.value?.id }),
-        });
+const customerId = 'cus_SppxMS2MA5aEBd'
+const { user, isloaded } = useUser();
 
-        if (!res.ok) {
-            plan.value = false;
-        } else {
-            const data = await res.json();
-            plan.value = data?.plan || false;
-        }
+async function fetchCustomerData() {
 
-        loaded.value = true;
-        
-    } catch (err) {
-    
-        console.error('Erreur lors de la récupération du plan :', err);
-        plan.value = false;
-        loaded.value = true;
-        clearInterval(interval);
-    
-    } finally {
-        clearInterval(interval);
-    }
+  try {
 
+    const loaded = await isloaded;
+    console.log(await loaded);
+
+    const res = await fetch(`http://localhost:3000/money/customer/${customerId}/${user.value.id}`);
+
+    if (!res.ok) throw new Error(`Erreur API: ${res.status}`);
+    const data = await res.json();
+
+    localuser.value = data.localuser;
+    customer.value = data.customer;
+    paymentMethods.value = data.paymentMethods || [];
+    subscriptions.value = data.subscriptions || [];
+
+    plan_color.value = localuser.value.plan.color || 'var(--btn)';
+
+  } catch (err) {
+    error.value = err.message;
+
+  } finally {
+    loading.value = false;
   }
-  
-}, 1000);
+
+}
+
+onMounted(fetchCustomerData);
 
 
 </script>
+
