0d157424d1b655e47765731b99c44af960b947c0
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-10-24 11:34:11 +0200

    Update salert.ts

--- src/assets/ts/salert.ts
+++ src/assets/ts/salert.ts
@@ -1,24 +1,28 @@
 import Swal, { type SweetAlertIcon, type SweetAlertOptions } from 'sweetalert2'
 
-export function salert(text: string, type: SweetAlertIcon): void {
+export class salert {
+    
+    constructor(text: string, type: SweetAlertIcon) {
 
-    const options: SweetAlertOptions = {
-        title: 'SilverTransfert',
-        html: text,
-        icon: type,
-        confirmButtonText: 'Fermer',
-        background: '#f0f0f0',
-        color: '#333',
-        confirmButtonColor: '#7f6cff',
-        timer: (789 * 789)*789,
-        showClass: {
-            popup: 'animate__animated animate__fadeIn'
-        },
-        hideClass: {
-            popup: 'animate__animated animate__fadeOut'
+        const options: SweetAlertOptions = {
+            title: 'SilverNote',
+            html: text,
+            icon: type,
+            confirmButtonText: 'Fermer',
+            background: 'var(--bg2)',
+            color: 'var(--text)',
+            confirmButtonColor: 'var(--btn)',
+            timer: (789 * 789)*789,
+            showClass: {
+                popup: 'animate__animated animate__fadeIn'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOut'
+            }
         }
-    }
 
-    Swal.fire(options);
+        Swal.fire(options);
+
+    }
 
 }
