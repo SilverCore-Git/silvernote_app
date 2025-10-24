6d09450cb4618eecaefa713c08090ad0c4875fea
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-10-24 11:08:20 +0200

    Create UserPlan.ts

---
+++ src/assets/ts/user/UserPlan.ts
@@ -0,0 +1,51 @@
+import { ref, watchEffect } from "vue";
+import { useUser } from "@clerk/vue";
+import { api_url } from "../backend_link";
+import type { Plan } from "../type";
+
+
+
+export function useUserPlan() {
+
+    const { user, isLoaded } = useUser();
+    const userPlan = ref<Plan | null>(null);
+    const loading = ref<boolean>(true);
+    const error = ref<string | null>(null);
+
+    async function getUserPlan() {
+
+        if (!user.value?.id) return null;
+
+        try {
+
+            const res = await fetch(`${api_url}/user/plan/get`, {
+                headers: { 
+                    "Content-Type": "application/json" 
+                },
+                body: JSON.stringify({ userId: user.value.id }),
+            });
+
+            if (!res.ok) throw new Error(`Erreur API (${res.status})`);
+            const data = await res.json();
+            return data;
+
+        } catch (err: any) {
+            console.error("Erreur getUserPlan:", err.message);
+            error.value = err.message;
+            return null;
+        }
+
+    }
+
+    watchEffect(async () => {
+        if (isLoaded.value && user.value) {
+            loading.value = true;
+            userPlan.value = await getUserPlan();
+            loading.value = false;
+        }
+    });
+
+
+    return { plan: userPlan, loading, error, refresh: getUserPlan };
+
+}
