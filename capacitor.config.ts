7966301e5d584e5638907c3df2362d5d5a4411a5
m335 <marien.vienot@gmail.com>
2025-11-16 21:40:32 +0100

    updated capacitor conf

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config = {
   appId: 'fr.silvercore.silvernote',
   appName: 'silverNote',
   webDir: 'dist',
-  server: {
-    url: 'https://app.silvernote.fr',
-    cleartext: false
-  }
 };
 
 export default config;
