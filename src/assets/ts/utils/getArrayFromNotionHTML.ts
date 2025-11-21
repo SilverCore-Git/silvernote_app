c673f7e0221bbee3ba2aabfe70b03fa68d8979ce
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-11-21 22:38:01 +0100

    add notion file import support

---
+++ src/assets/ts/utils/getArrayFromNotionHTML.ts
@@ -0,0 +1,50 @@
+import utils from "../utils";
+
+export default function parseNotionHTML
+(html: string): { title: string; icon: string; content: string } 
+{
+
+    let res: { title: string; icon: string; content: string };
+
+    try {
+
+        res = {
+            title: '',
+            icon: '',
+            content: ''
+        }
+
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+
+        
+        const emoji_icon_span = doc.querySelector('span.icon');
+
+        if (emoji_icon_span)
+        {
+            res.icon = utils.emojiToBase64(emoji_icon_span.textContent);
+        }
+
+        const title = doc.querySelector('h1.page-title');
+
+        if (title)
+        {
+            res.title = title.textContent;
+        }
+
+        const content = doc.querySelector('div.page-body');
+
+        if (content) 
+        {
+            res.content = content.innerHTML;
+        }
+
+    }
+
+    catch(err) {
+        throw new Error(`Error on parse html from notion : ${err}`);
+    }
+
+    return res;
+
+}
