d5ba1ffc1771abd90974673a851a5280de276b98
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-12-02 18:15:55 +0100

    add handle image upload template

---
+++ src/components/Markdown/tiptap-extensions/image-upload-node/index.ts
@@ -0,0 +1,11 @@
+import imageUploadNode from "./image-upload-node-extension"; 
+import handleImageUpload from "./handleImageUpload";
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5mo
+
+export {
+    imageUploadNode,
+    handleImageUpload,
+    MAX_FILE_SIZE
+}
+
