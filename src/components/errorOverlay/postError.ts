5cebf697decf5dea7b168467c99a4ee1d9bd4848
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-12-18 18:26:45 +0100

    update errorOverlay

--- src/components/errorOverlay/postError.ts
+++ src/components/errorOverlay/postError.ts
@@ -3,22 +3,53 @@ import type { AppError } from "./types";
 import sentErrorToDiscord from "./sentErrorToDiscord";
 
 const errors = ref<AppError[]>([]);
-
 const activeErrorIndex = ref<number | null>(null);
 
-const postError = (err: Omit<AppError, "id" | "timestamp" | "show">) => {
-    const newError: AppError = {
-        id: crypto.randomUUID(),
-        timestamp: new Date(),
-        show: true,
-        little: false,
-        ...err,
-    };
+function extractMessage(err: unknown): string {
+  if (err instanceof DOMException) {
+    return `${err.name}: ${err.message}`;
+  }
 
-    errors.value.push(newError);
-    activeErrorIndex.value = errors.value.length - 1;
+  if (err instanceof Error) {
+    return err.message;
+  }
 
-    sentErrorToDiscord(newError);
+  if (typeof err === "string") {
+    return err;
+  }
+
+  try {
+    return JSON.stringify(err);
+  } catch {
+    return "Erreur inconnue";
+  }
+}
+
+const postError = (params: {
+  message?: string;
+  place?: string;
+  error?: "400" | "500" | "unknow";
+  more?: string;
+  raw?: unknown;
+}) => {
+  const finalMessage = params.message ?? extractMessage(params.raw);
+
+  const newError: AppError = {
+    id: crypto.randomUUID(),
+    timestamp: new Date(),
+    show: true,
+    little: false,
+
+    message: finalMessage,
+    place: params.place,
+    error: params.error ?? "unknow",
+    more: params.more,
+  };
+
+  errors.value.push(newError);
+  activeErrorIndex.value = errors.value.length - 1;
+
+  sentErrorToDiscord(newError);
 };
 
 const closeError = (index: number) => {
@@ -27,7 +58,6 @@ const closeError = (index: number) => {
   }
 };
 
-
-export { closeError, errors, activeErrorIndex, postError }
+export { closeError, errors, activeErrorIndex, postError };
 export type { AppError };
 export default postError;
