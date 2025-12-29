bb45c18627889530e87dac86bc033a6e9341a701
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-12-29 22:38:11 +0100

    create UserAvatarButton with hes child components

---
+++ src/components/user/UserAvatar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div @click="emit('emitClick')">
+        <UserAvatar
+            :appearance="{
+                elements: {
+                    userAvatarBox: {
+                        width: '24px',
+                        height: '24px',
+                        borderRadius: '50%'
+                    }
+                }
+            }"
+        />
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import { UserAvatar } from '@clerk/vue'; 
+
+const emit = defineEmits([
+    "emitClick"
+])
+
+</script>
---
+++ src/components/user/UserAvatarButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { UserProfile } from '@clerk/vue';
+import UserAvatar from './UserAvatar.vue';
+import BackdropOverlay from '../common/BackdropOverlay.vue';
+import { ref } from 'vue';
+
+const openProfile = ref<boolean>(false);
+</script>
+
+<template>
+
+    <UserAvatar
+        @emit-click="openProfile = !openProfile"
+    />
+
+    <Teleport to="body">
+        <div v-if="openProfile">
+            <div class="fixed inset-0 z-100 flex justify-center items-center">
+                <UserProfile />
+            </div>
+            <BackdropOverlay @emit-click="openProfile = false" />
+        </div>
+    </Teleport>
+
+</template>
