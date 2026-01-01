11106462049b32211de382ed2a783119254753ee
MisterPapaye <philippeetchtebaise@hotmail.com>
2026-01-01 18:54:57 +0100

    pass to an html index

--- src/windows/main/mainWindow.js
+++ src/windows/main/mainWindow.js
@@ -1,5 +1,7 @@
-const { BrowserWindow, Menu, globalShortcut } = require("electron");
+const { BrowserWindow, Menu, globalShortcut, ipcMain } = require("electron");
 const Console = require("../../assets/logger");
+const path = require("path");
+const packageJson = require("../../../package.json");
 
 module.exports = function create_main_window() {
   return new Promise((resolve, reject) => {
@@ -10,8 +12,8 @@ module.exports = function create_main_window() {
         frame: false,
         webPreferences: {
           nodeIntegration: true,
-          contextIsolation: true,
-          disableHardwareAcceleration: true,
+          contextIsolation: false,
+          webviewTag: true,
         },
         show: false,
       });
@@ -20,12 +22,54 @@ module.exports = function create_main_window() {
 
       Menu.setApplicationMenu(null);
 
-      window.loadURL("https://app.silvernote.fr");
+      window.loadFile(path.join(__dirname, "./index.html"));
+
+      // Activer les popups
+      window.webContents.setWindowOpenHandler(({ url }) => {
+        // Ici on ouvre dans une nouvelle BrowserWindow
+        const popup = new BrowserWindow({
+          width: 800,
+          height: 600,
+          parent: window, // optionnel : fenêtre enfant
+          modal: false,   // pas modal
+          webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            webviewTag: true,
+          },
+        });
+
+        popup.loadURL(url);
+        return { action: 'deny' }; // empêche Electron de créer sa propre popup
+      });
 
       globalShortcut.register("CommandOrControl+Shift+I", () => {
         if (window) {
           window.webContents.openDevTools({ mode: "detach" });
         }
+      });
+
+      // Écouteurs IPC pour les boutons de la titlebar
+      ipcMain.on('window-minimize', (event) => {
+        const win = BrowserWindow.fromWebContents(event.sender);
+        if (win) win.minimize();
+      });
+
+      ipcMain.on('window-maximize', (event) => {
+        const win = BrowserWindow.fromWebContents(event.sender);
+        if (win) {
+          if (win.isMaximized()) win.unmaximize();
+          else win.maximize();
+        }
+      });
+
+      ipcMain.on('window-close', (event) => {
+        const win = BrowserWindow.fromWebContents(event.sender);
+        if (win) win.close();
+      });
+
+      ipcMain.handle('get-version', () => {
+        return packageJson.version;
       });
 
       window.once("ready-to-show", () => {
