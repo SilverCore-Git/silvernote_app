797b341c99745dd589814d0dcc16497880644ad9
MisterPapaye <philippeetchtebaise@hotmail.com>
2026-01-27 17:13:16 +0100

    create icon loader

---
+++ src/components/iconLoader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="h-full w-full flex justify-center items-center">
+        <img src="/favicon.svg" class="loader-icon" />
+    </div>
+</template>
+
+<style scoped>
+
+.loader-icon {
+    width: 80px;
+    height: 80px;
+    animation: flash 2.5s ease-in-out infinite;
+}
+
+@keyframes flash {
+    0% { opacity: 0.5; }
+    50% { opacity: 1; }
+    100% { opacity: 0.5; }
+}
+
+</style>
