1f4e99949e269d05c460abf31e8e31e04d128fd9
mister_papaye <philippeetchtebaise@hotmail.com>
2026-02-04 12:07:03 +0100

    add 2048 game to env

---
+++ src/components/2048/App2048.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import Grid from './components/Grid.vue';
+</script>
+
+<template>
+  <div
+    class="
+      flex justify-center items-center 
+      max-w-[520px] relative
+    "
+  >
+    <grid />
+  </div>
+</template>
+
