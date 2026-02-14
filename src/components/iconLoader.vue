c1b64659b2d960ecface0344bb4799145b4a7666
MisterPapaye <philippeetchtebaise@hotmail.com>
2026-02-14 11:12:04 +0100

    update loader

--- src/components/iconLoader.vue
+++ src/components/iconLoader.vue
@@ -1,15 +1,57 @@
 <template>
     <div class="h-full w-full flex justify-center items-center">
-        <img src="/favicon.svg" class="loader-icon" />
+        <div class="loader-wrapper">
+            <img src="/favicon.svg" class="loader-icon" />
+        </div>
     </div>
 </template>
 
 <style scoped>
 
+.loader-wrapper {
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 12px;
+}
+
 .loader-icon {
     width: 80px;
     height: 80px;
     animation: flash 2.5s ease-in-out infinite;
+    display: block;
+}
+
+.loader-wrapper::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: -150%;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(
+        to right,
+        transparent,
+        rgba(255, 255, 255, 0.3),
+        transparent
+    );
+    transition: 0.8s;
+    z-index: 1;
+    animation: balayage 4s ease-in-out infinite;
+}
+
+@keyframes balayage {
+    0% {
+        left: -150%;
+    }
+    33.33% {
+        left: 150%;
+    }
+    100% {
+        left: 150%;
+    }
 }
 
 </style>
