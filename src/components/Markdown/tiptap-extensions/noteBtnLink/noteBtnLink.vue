4ab35f9d232b21833117faf2ffea1bc555ab4f3c
MisterPapaye <philippeetchtebaise@hotmail.com>
2026-02-24 19:40:48 +0100

    fix lien vers une note

--- src/components/Markdown/tiptap-extensions/noteBtnLink/noteBtnLink.vue
+++ src/components/Markdown/tiptap-extensions/noteBtnLink/noteBtnLink.vue
@@ -1,7 +1,7 @@
 <template>
 
     <SearchANote
-        v-if="note?.id == -3"
+        v-if="note?.uuid === 'new'"
         @note="setNote"
     />
 
@@ -9,9 +9,9 @@
         
         <button
             class="
-                    flex items-center gap-2 px-3 py-1 rounded-lg my-3
-                    bg-gray-100 hover:bg-gray-200 cursor-pointer
-                "
+                flex items-center gap-2 px-3 py-1 rounded-lg my-3
+                bg-(--bg2)/70 hover:bg-(--bg2) text-(--text) cursor-pointer
+            "
             @click="handleClick"
         >
             <img v-if="note?.icon" :src="note.icon" class="w-6 h-6" />
@@ -28,7 +28,7 @@
 import SearchANote from '@/components/notes/searchANote.vue'
 import { NodeViewWrapper, nodeViewProps } from '@tiptap/vue-3'
 import { onMounted, ref } from 'vue'
-import {  useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
 import { saveNote } from '../../Function/saveNote'
 import { Notes } from '@/assets/ts/database/Var'
 
@@ -36,32 +36,57 @@ const props = defineProps({
     ...nodeViewProps,
 })
 
-const router = useRouter();
-const note = ref<{ title: string; icon?: string, id: number } | null>(null);
+const router = useRouter()
 
-onMounted(async () => {
-    note.value = { title: '', id: -1, icon: '' }
+type NoteType = {
+    title: string
+    icon?: string
+    uuid: string
+}
 
-    if (props.node.attrs.noteId === -3) {
-        note.value.id = -3;
-    } else {
+const note = ref<NoteType | null>(null)
+
+onMounted(() => {
+
+    // valeur par défaut
+    note.value = { title: '', uuid: 'new', icon: '' }
+
+    if (props.node.attrs.noteId === 'new') 
+    {
+        note.value.uuid = 'new'
+    } 
+    else 
+    {
         try {
-            note.value = Notes.value.find(note => note.uuid == props.node.attrs.noteId) as { title: string, icon?: string, id: number }
+            const foundNote = Notes.value.find(
+                (n) => n.uuid === props.node.attrs.noteId
+            ) as NoteType | undefined
+
+            if (foundNote) {
+                note.value = foundNote
+            }
         } catch (e) {
             console.error('Erreur récupération note :', e)
         }
     }
+
 })
 
 const setNote = async (noteId: string) => {
-    note.value = Notes.value.find(note => note.uuid === noteId) as any;
-    props.updateAttributes({ noteId });
-    await saveNote(noteId);
+    
+    const foundNote = Notes.value.find(n => n.uuid === noteId)
+
+    if (!foundNote) return
+
+    note.value = foundNote
+    props.updateAttributes({ noteId })
+    await saveNote(noteId)
+
 }
 
-const handleClick = async () => {
-    if (!note.value) return;
-    router.push(`/edit/${note.value.id}`)
+const handleClick = () => {
+    if (!note.value) return
+    router.push(`/edit/${note.value.uuid}`)
 }
 
 </script>
