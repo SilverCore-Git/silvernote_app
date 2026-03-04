01dcdc009fe55c715cdcf52feb8de88d48b20b1b
MisterPapaye <philippeetchtebaise@hotmail.com>
2026-03-04 10:49:21 +0100

    create dbSocket listener

--- src/assets/ts/database/dbSocket.ts
+++ src/assets/ts/database/dbSocket.ts
@@ -1,11 +1,66 @@
 import type { Socket } from "socket.io-client";
 import type { Ref } from "vue";
+import type { Note, Tag } from "../type";
+import { Notes, Tags } from "./Var";
 
 
 export default function (socket: Ref<Socket>)
 {
 
 
+    socket.value.on('connect', () => {
+        socket.value.emit('user-connect');
+    });
+
+
+
+    // sync notes
+    
+    socket.value.on('note:create', (note: Note) => {
+        
+        Notes.value.push(note);
+
+    });
+
+    socket.value.on('note:update', (note: Note) => {
+
+        const index = Notes.value.findIndex(n => n.uuid === note.uuid);
+        if (index !== -1) {
+            Notes.value[index] = note;
+        }
+
+    })
+
+    socket.value.on('note:delete', (note: Note) => {
+
+        Notes.value = Notes.value.filter(n => n.uuid !== note.uuid);
+
+    })
+
+
+
+    // sync tags
+
+    socket.value.on('tag:create', (tag: Tag) => {
+        
+        Tags.value.push(tag);
+
+    });
+
+    socket.value.on('tag:update', (tag: Tag) => {
+
+        const index = Tags.value.findIndex(n => n.uuid === tag.uuid);
+        if (index !== -1) {
+            Tags.value[index] = tag;
+        }
+
+    })
+
+    socket.value.on('tag:delete', (tag: Tag) => {
+
+        Tags.value = Tags.value.filter(n => n.uuid !== tag.uuid);
+
+    })
 
 }
 
